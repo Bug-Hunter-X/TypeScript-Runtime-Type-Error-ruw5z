734da30018609cf4f64cb6491bcc4fa5516829d5
BugHunter-X <askzenai@gmail.com>
2024-12-07 14:22:06 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,9 @@
+function add(a: number, b: number): number {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Inputs must be numbers');
+  }
+  return a + b;
+}
+
+let result = add(1, "2"); // Throws an error at runtime
+console.log(result); 
